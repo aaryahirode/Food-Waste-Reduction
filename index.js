7f2e93fa04286db092edbcdce0eececdc8161b5f
aaryahirode <aaryahirode@gmail.com>
2025-02-15 14:55:53 +0530

    Create contact form

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-import express from "express";
+import express from "express"
+import ngrok from "ngrok"
+import pg from "pg"
+import multer from "multer"
+
+const app = express()
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(port, async () => {
+    console.log(`Server running on http://localhost:${port}`);
+    try {
+      const url = await ngrok.connect(port);
+      console.log(`Ngrok public URL: ${url}`);
+    } catch (err) {
+      console.error("Error starting ngrok:", err);
+    }
+});
+
+//Homepage
+app.get("/",(req,res)=>{
+    res.render("homepage.ejs") 
+})
+
+
+//Contact Us
+app.get("/contact",(req,res)=>{
+    res.render("contact.ejs");
+  })
