d78d869bfcb02972fc569737b15b1a57aa8469f7
OmGupta1719 <guptaom1719@gmail.com>
2025-02-15 11:52:04 +0530

    Create homepage render

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express"
+import ngrok from "ngrok"
+import pg from "pg"
+import multer from "multer"
+
+const app = express()
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.listen(port, async () => {
+    console.log(`Server running on http://localhost:${port}`);
+    try {
+      const url = await ngrok.connect(port);
+      console.log(`Ngrok public URL: ${url}`);
+    } catch (err) {
+      console.error("Error starting ngrok:", err);
+    }
+});
+
+//Homepage
+app.get("/",(req,res)=>{
+    res.render("homepage.ejs") 
+})
